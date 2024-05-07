1d3304a92266cc36ba6319b2544014f193faf696
sanikodesolution <127325935+sanikodesolution@users.noreply.github.com>
2024-05-07 16:25:42 +0600

    Add files via upload

---
+++ PriceRangeSlider.vue
@@ -0,0 +1,27 @@
+<template>
+<MultiRangeSlider :min="0" :max="100" :min-value="sBarMinValue" :max-value="sBarMaxValue" :step="1" :range-margin="0" @input="update_sBarValues" />
+</template>
+<script>
+import { ref } from "vue";
+import MultiRangeSlider from "multi-range-slider-vue";
+
+export default {
+  name: "App",
+  components: {
+    MultiRangeSlider
+  },
+  data: () => {
+    return {
+      videoIsOpen: false,
+      sBarMinValue: 0,
+      sBarMaxValue: 45,
+    };
+  },
+  methods: {
+    update_sBarValues(e) {
+      this.sBarMinValue = e.minValue;
+      this.sBarMaxValue = e.maxValue;
+    }
+  }
+};
+</script>
